c4e470784080a8fce7d589fee5fa55bbecc05bc3
Ziv Aricha <zivaricha@gmail.com>
2018-01-07 14:17:54 +0000

    remove duplicated function

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,5 @@ class Post < ActiveRecord::Base
   validates :identifier, uniqueness: true
   
   scope :facebook_maximum, -> { where(social_media: FACEBOOK).maximum("original_created_time")}
-  def self.get_channel_name(channel_url = "")
-    channel_url.split("/").last if channel_url.present?
-  end
+
 end
